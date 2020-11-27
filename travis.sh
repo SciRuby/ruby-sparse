22ed0ccff58b7673461fad5132a72b4ede651fee
uditgulati <uditgulati0@gmail.com>
2020-11-28 02:48:23 +0530

    add travis support

---
+++ travis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+if [ "$1" = "install" ]
+then
+  bundle install
+  bundle exec rake compile
+fi
+
+if [ "$1" = "before_install" ]
+then
+  case "$TRAVIS_OS_NAME" in
+    linux)
+      sudo apt-get update -qq
+      ;;
+  esac
+
+  if [ $TRAVIS_RUBY_VERSION == '2.2.4' ] || [ $TRAVIS_RUBY_VERSION == '2.1.8' ]
+  then
+    gem install --no-document bundler -v '~> 1.6';
+    gem install --no-document parallel -v '1.13.0';
+  else
+    gem install --no-document bundler;
+  fi
+
+fi
+
+if [ "$1" = "script" ]
+then
+  bundle exec rake test
+fi
